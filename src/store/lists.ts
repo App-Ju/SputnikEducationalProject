a5e3c5456ad8837a82df3e3bae92b81ab4711043
App-Ju <appjuice@mail.ru>
2022-04-21 15:37:14 +0700

    fixed display of lists for the current board

--- src/store/lists.ts
+++ src/store/lists.ts
@@ -1,15 +1,13 @@
 import { defineStore } from "pinia";
 import List from "@/models/List.model";
-import Task from "@/models/Task.model";
 
 export const useListsStore = defineStore({
   id: "lists",
-  state: () => ({ lists: [] as List[] }),
-  getters: {
-    currentBoardsList: (state) => (boardId: number) => {
-      return state.lists.filter((el) => el.boardId === boardId);
-    },
-  },
+  state: () => ({
+    lists: [] as List[],
+    currentBoardLists: [] as List[],
+  }),
+  getters: {},
   actions: {
     addListName(boardId: number, id: number, name: string) {
       this.lists.push(new List(boardId, id, name, []));
@@ -21,6 +19,11 @@ export const useListsStore = defineStore({
     deleteList(id: number) {
       const index = this.lists.findIndex((el) => el.id === id);
       this.lists.splice(index, 1);
+    },
+    changeCurrentBoardStore(boardId: number) {
+      this.currentBoardLists = this.lists.filter(
+        (el) => el.boardId === boardId
+      );
     },
   },
 });
